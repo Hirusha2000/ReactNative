329685334f6bc7d162ac7efb8f81f820d51cecdf
Hirusha2000 <2000hirushaudara@gmail.com>
2024-01-12 13:33:28 +0530

    login page ui updates

--- App.tsx
+++ App.tsx
@@ -1,5 +1,50 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, Text, TextInput } from 'react-native';
+
+
+
+function LoginField(){
+
+  return(
+<View style={{marginTop:40}}>
+<View style={{ backgroundColor:'white',
+borderRadius:20,
+height:60,
+marginHorizontal:30,
+justifyContent:'center',
+paddingLeft:20
+}}>
+<TextInput placeholder='Your Email' 
+placeholderTextColor={'#265CA5'}
+style={{
+  fontSize:20
+}}
+/>
+
+</View>
+
+<View style={{ backgroundColor:'white',
+borderRadius:20,
+height:60,
+marginHorizontal:30,
+justifyContent:'center',
+paddingLeft:20,
+marginTop:20
+}}>
+<TextInput placeholder='Password' 
+placeholderTextColor={'#265CA5'}
+style={{
+  fontSize:20
+}}
+/>
+
+</View>
+
+</View>
+
+  );
+}
+
 
 function App(): React.JSX.Element {
   return (
@@ -9,6 +54,9 @@ function App(): React.JSX.Element {
         source={require('./assets/img/balo.jpg')}
         resizeMode='cover'
       />
+
+      <Text style={styles.welText}>{'Welcome\nBack'}</Text>
+      <LoginField/>
     </View>
   );
 }
@@ -19,10 +67,21 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   image: {
+    position:'absolute',
     flex: 1,
     width: '100%',
     height: '100%',
   },
+
+  welText:{
+fontSize:50,
+color:'#3A8BF8',
+fontWeight:'600',
+marginTop:120,
+marginLeft:30
+
+
+  }
 });
 
 export default App;
