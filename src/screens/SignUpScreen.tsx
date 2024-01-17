499b874c6b417c394e5d1ee4183cd6ca30856169
Hirusha2000 <2000hirushaudara@gmail.com>
2024-01-17 19:59:44 +0530

    new user login part with firebase

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,11 +1,47 @@
-import { Image, StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, Alert, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { Icon } from '@rneui/themed';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../../firebase/firebaseinit';
+import { useNavigation } from '@react-navigation/native';
 
 
 
 function SignUpSection(){
+
+// This called state varibles
+  const [name , setName]= useState('');
+  const [email , setEmail]= useState('');
+  const [password, setPassword]= useState('');
+
+  const[saving,setSaving]=useState(false);
+
+  const nav:any = useNavigation();
+
+  function saveUser(){
+
+    setSaving(true);
+
+   // Alert.alert(name+" "+email+" "+password);
+
+    addDoc (collection(db,'Users'),{
+name:name,
+email:email,
+password:password
+
+// key value 2 ma samana nm kamathi nm mekata name witarak denna unath puluwan.
+    }).then(t=>{
+      setSaving(false);
+      Alert.alert("User is successfully registerd!")
+      nav.navigate('Login');
+    }).catch(e=>{
+      setSaving(false);
+      console.error("User account creation failed!");
+      
+    })
+
+  }
 
     return(
   <View style={{marginTop:100}}>
@@ -16,7 +52,7 @@ function SignUpSection(){
   justifyContent:'center',
   paddingLeft:20
   }}>
-  <TextInput placeholder='Name' 
+  <TextInput onChangeText={(v)=>setName(v)} placeholder='Name' 
   placeholderTextColor={'#666'}
   style={{
     fontSize:15, color:'black'
@@ -33,7 +69,7 @@ function SignUpSection(){
   paddingLeft:20,
   marginTop:20
   }}>
-  <TextInput placeholder='Your Email' 
+  <TextInput  onChangeText={(v)=>setEmail(v)} placeholder='Your Email' 
   placeholderTextColor={'#666'}
   style={{
     fontSize:15, color:'black'
@@ -50,7 +86,8 @@ function SignUpSection(){
   paddingLeft:20,
   marginTop:20
   }}>
-  <TextInput placeholder='Password' 
+  <TextInput onChangeText={(v)=>setPassword(v)} placeholder='Password' 
+    secureTextEntry={true}
   placeholderTextColor={'#666'}
   style={{
     fontSize:15, color:'black'
@@ -58,7 +95,7 @@ function SignUpSection(){
   />
   
   </View>
- <SingnUpButton/>
+ <SingnUpButton sUser={saveUser} saving={saving}/>
   <BottomSection/>
   </View>
   
@@ -68,7 +105,7 @@ function SignUpSection(){
 
 
   
-function SingnUpButton() {
+function SingnUpButton(p:any) {
     return(
       <View style={{flexDirection:'row' ,marginTop:120}}>
   
@@ -76,7 +113,7 @@ function SingnUpButton() {
         <View style={{height:70, flex:1, justifyContent:'center'}}>
   <Text style={{fontSize:25, color:'white',marginLeft:100,fontWeight:'800'}}>Sign Up</Text>
         </View>
-        <View style={{height:70, flex:1,justifyContent:'center',alignItems:'flex-end'}}>
+        {/* <View style={{height:70, flex:1,justifyContent:'center',alignItems:'flex-end'}}>
   
   <View style={{width:50,height:50,backgroundColor:'#367cfe',marginRight:130 ,borderRadius:100
   ,justifyContent:'center',alignItems:'center'}}>
@@ -84,7 +121,30 @@ function SingnUpButton() {
   
   </View>
   
-        </View>
+        </View> */}
+
+
+<TouchableOpacity activeOpacity={0.7} onPress={p.sUser}>
+
+<View style={{width:50,height:50,backgroundColor:'#367cfe',marginRight:130 ,borderRadius:100
+ ,justifyContent:'center',alignItems:'center',marginTop:10}}>
+
+   {
+
+     (p.saving)?<ActivityIndicator color='white'/>
+     :<Icon size={50} color={'white'} name={'chevron-right'} type='FontAwesome'/>
+   }
+ {/* <Icon size={50} color={'white'} name={'chevron-right'} type='FontAwesome'/> */}
+
+
+
+
+
+
+ 
+ </View>
+</TouchableOpacity>
+ 
        
       </View>
     );
